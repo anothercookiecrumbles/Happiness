190f14b98914f89f0991fed942339b008e502144
anothercookiecrumbles <priyanjana@gmail.com>
2015-06-06 18:09:59 +0100

    Demo from lecture 5.

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Priyanjana Bengani on 06/06/2015.
+//  Copyright (c) 2015 anothercookiecrumbles. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+    
+}
